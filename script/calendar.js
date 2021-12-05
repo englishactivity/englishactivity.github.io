030344d4ddfe75c18d1422e391d466e615cb0088
englishactivity <95325742+englishactivity@users.noreply.github.com>
2021-12-04 21:29:22 -0300

    Update calendar.js

--- script/calendar.js
+++ script/calendar.js
@@ -83,6 +83,8 @@ var diasMaxMes = [
       getStudentText(userCurrent.uid,d);
     }else{
       reference = "Students/" + currentStudent.id + '/' + ano + '/' + mes + '/' + d;
+      getTeacherText(userCurrent.uid,d);
+      getStudentText(userCurrent.uid,d);
     }
     firebase.storage().ref(reference).getDownloadURL().then(function(url) {
       var xhr = new XMLHttpRequest();
